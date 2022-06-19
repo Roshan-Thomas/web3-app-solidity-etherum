06b758212d8e3fc66bf4bf35841507db6b0fef17
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-19 23:13:36 +0300

    first commit - compile contract locally and run it

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDA-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
